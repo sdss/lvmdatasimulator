7380b3d16fd600e89fe6f8053c100ba9f6ad72cc
cloud182 <enry91con@gmail.com>
2021-11-15 13:07:48 -0300

    Added the stars.py file

---
+++ python/lvmdatasimulator/stars.py
@@ -0,0 +1,11 @@
+# encoding: utf-8
+#
+# @Author: Oleg Egorov, Enrico Congiu
+# @Date: Nov 15, 2021
+# @Filename: field.py
+# @License: BSD 3-Clause
+# @Copyright: Oleg Egorov, Enrico Congiu
+
+
+class StarList:
+    
