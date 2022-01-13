9f3ef06f3ff4b61c8f6bdd84f95211fdcc80cef2
cloud182 <enry91con@gmail.com>
2022-01-13 11:51:41 +0100

    Added the observation.py file

---
+++ python/lvmdatasimulator/observation.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+#
+# @Author: Oleg Egorov, Enrico Congiu
+# @Date: Nov 12, 2021
+# @Filename: field.py
+# @License: BSD 3-Clause
+# @Copyright: Oleg Egorov, Enrico Congiu
+
+"""
+Simple class to store all the information regarding the observations relevant for the
+simulations
+"""
