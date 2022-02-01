10a058b7fc26e30f0e29745675777211682e28a6
cloud182 <enry91con@gmail.com>
2022-02-01 15:20:51 -0300

    creation of utils.py

---
+++ python/lvmdatasimulator/utils.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+#
+# @Author: Oleg Egorov, Enrico Congiu
+# @Date: Nov 12, 2021
+# @Filename: field.py
+# @License: BSD 3-Clause
+# @Copyright: Oleg Egorov, Enrico Congiu
+
+import numpy as np
+
+def round_up_to_odd(f):
+    return np.ceil(f) // 2 * 2 + 1
